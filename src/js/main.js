4e6b25d88d73c0151adf06bd3646bf072be2f409
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2024-02-06 14:06:58 -0700

    Finished the customizable alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import productList from "./productList.mjs";
+import { renderAlerts } from "./alertModule.js";
 
-productList(".product-list", "tents");
+async function init() {
+  await renderAlerts(); // Render alerts before rendering product list
+  productList(".product-list", "tents");
+}
+
+init();
